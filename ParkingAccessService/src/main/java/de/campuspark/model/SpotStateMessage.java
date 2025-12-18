fccd6090cae99919389f93f8daba79522cf503a2
DrSommer20 <timmi.sommer@gmail.com>
2025-12-18 14:30:35 +0100

    Refactoring of the SpotStateMessage for diplaying simpler TimeStamps

--- ParkingAccessService/src/main/java/de/campuspark/model/SpotStateMessage.java
+++ ParkingAccessService/src/main/java/de/campuspark/model/SpotStateMessage.java
@@ -1,7 +1,11 @@
 package de.campuspark.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+
+import de.campuspark.service.CalendarService;
+
 import java.time.Instant;
+import java.time.ZoneId;
 
 // JsonInclude.NON_NULL sorgt dafür, dass 'null' Felder (wie departureTime) im JSON weggelassen werden
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -20,7 +24,7 @@ public class SpotStateMessage {
         this.spotId = spotId;
         this.state = state;
         this.plate = plate;
-        this.arrivalTime = arrival != null ? arrival.toString() : null;
-        this.estimatedDepartureTime = departure != null ? departure.toString() : null;
+        this.arrivalTime = arrival != null ? arrival.atZone(ZoneId.systemDefault()).toLocalTime().format(CalendarService.timeFormatter) : null;
+        this.estimatedDepartureTime = departure != null ? departure.atZone(ZoneId.systemDefault()).toLocalTime().format(CalendarService.timeFormatter) : null;
     }
 }
